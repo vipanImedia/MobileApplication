10a23ade493291d8524f1c5786a5a69ca201419e
vipanImedia <vipan@techmadly.com>
2016-09-08 15:19:06 +0530

    First Commit

---
+++ app/src/main/java/imediadesign/com/githubdemo/MainActivity.java
@@ -0,0 +1,15 @@
+package imediadesign.com.githubdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(),"Hello",Toast.LENGTH_SHORT).show();
+    }
+}
